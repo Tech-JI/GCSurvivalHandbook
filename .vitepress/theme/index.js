78a28e682c77208fdb19583598991894611122d7
Jacky Tang <tang.jie@live.com>
2026-08-01 01:17:50 +0800

    feat: add giscus comments and PWA support
    - Add giscus comment section below prev/next navigation via doc-after
      slot; zh/en versions share one discussion thread per page using
      specific-term mapping
    - Add PWA support with @vite-pwa/vitepress: manifest, service worker,
      and generated app icons (192/512) plus SVG favicon

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -1,9 +1,11 @@
 import DefaultTheme from "vitepress/theme-without-fonts";
+import Layout from "./layout/Layout.vue";
 import "./my_font.css";
 import ProgramFilter from "./components/ProgramFilter.vue";
 
 export default {
   extends: DefaultTheme,
+  Layout,
   enhanceApp({ app }) {
     app.component("ProgramFilter", ProgramFilter);
   },
